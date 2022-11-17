a3098eb75ae0655ebddb6e07b85f734bf9bacaf4
Nate-yu <2242936124@qq.com>
2022-11-17 17:23:01 +0800

    优化插入排序

--- source/c_practice/插入排序.c
+++ source/c_practice/插入排序.c
@@ -1,16 +1,15 @@
 #include "header.h"
 
 void insertSort(int arr[], int len){
-    if(len < 2) return;
-    int i,j,temp;
-    for(i = 1; i < len; i++) {
-    	temp = arr[i];
-    	for(j = i-1; j >= 0; j--) {
-    		if(arr[j] <= temp) break;
-    		arr[j+1] = arr[j];
-    	}
-    	arr[j+1] = temp;
-    }
+    for(int i = 1; i < len; i++) {
+		int temp = arr[i];
+		int j = i-1;
+		while(j >= 0 && arr[j] > temp) {
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j+1] = temp;
+	}
 }
 
 int main(int argc, char const *argv[])
